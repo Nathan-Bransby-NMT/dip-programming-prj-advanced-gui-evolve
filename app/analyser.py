2c9d8efa578c2aa2fb6cbd0a4b76279b6bed36ff
Nathan Bransby <141193650+Nathan-Bransby-NMT@users.noreply.github.com>
2024-05-28 17:22:35 +0800

    Doc: include script level documentation for

--- app/analyser.py
+++ app/analyser.py
@@ -1,1 +1,6 @@
+"""
+This srcipt is used to access pixel averaging functionality to analyse frames
+to identify when an IDE is active - reducing the cost factor for calling the
+AI APIs for identifying code.
+"""
 
